f85053b668e4bdb2e385854aa0ad99e87b60b04e
Pranav Waghmare <piyushw55@gmail.com>
2023-03-01 00:22:11 +0530

    [Pranav]Add: Ability to check First Name

---
+++ RegexProblem/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace RegexProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Well-Come to User Registration Validation Program");
+                Console.WriteLine("Enter the option number");
+                Console.WriteLine("1.Validate first name");
+                Console.WriteLine();
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Regexs regexs = new Regexs();
+                        regexs.ValidateFirstName("Pranav");
+                        Console.ReadKey();
+                        break;
+                    default:
+                        Console.WriteLine("please enter correct option");
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ RegexProblem/Regexs.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexProblem
+{
+    public class Regexs
+    {
+       
+        public void ValidateFirstName(string name)
+        {
+           
+            string firstName = "^[A-Z][a-z]{3,}?";
+
+            if(Regex.IsMatch(name,firstName))
+            {
+                Console.WriteLine("FirstName is Maching");
+            }
+            else
+            {
+                Console.WriteLine("FirstName is NOT Maching");
+            }
+        }
+
+       
+
+
+       
+    }
+}
