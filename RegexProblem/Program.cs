81dd6e8456c8368212c5925e73234b14801d60da
Pranav Waghmare <piyushw55@gmail.com>
2023-03-01 00:34:07 +0530

    [Pranav]Add: Ability to check Last Name

--- RegexProblem/Program.cs
+++ RegexProblem/Program.cs
@@ -4,20 +4,32 @@
     {
         static void Main(string[] args)
         {
+            Regexs regexs = new Regexs();
             bool flag = true;
             while (flag)
             {
                 Console.WriteLine("Well-Come to User Registration Validation Program");
                 Console.WriteLine("Enter the option number");
-                Console.WriteLine("1.Validate first name");
-                Console.WriteLine();
+                Console.WriteLine("1.Validate First name");
+                Console.WriteLine("1.Validate Last name");
+
+                Console.WriteLine("\t0 : EXIT");
+                Console.Write("ENTER YOUR CHOICE : ");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
                     case 1:
-                        Regexs regexs = new Regexs();
                         regexs.ValidateFirstName("Pranav");
                         Console.ReadKey();
+                        break;
+
+                    case 2:
+                        regexs.ValidateLastName("Waghmare");
+                        Console.ReadKey();
+                        break;
+
+                    case 0:
+                        flag= false;
                         break;
                     default:
                         Console.WriteLine("please enter correct option");
--- RegexProblem/Regexs.cs
+++ RegexProblem/Regexs.cs
@@ -25,9 +25,23 @@ namespace RegexProblem
             }
         }
 
-       
+        public void ValidateLastName(string name)
+        {
+            string lastName = "^[A-Z][a-z]{3,}?";
+
+            if (Regex.IsMatch(name, lastName))
+            {
+                Console.WriteLine("LastName is Maching");
+            }
+            else
+            {
+                Console.WriteLine("LastName is NOT Maching");
+            }
+        }
 
 
-       
+
+
+
     }
 }
